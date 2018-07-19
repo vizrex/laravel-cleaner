f1f9bab571842cd67d6c55c81facea6f6b6e56ef
Mian Zeshan Farooqi <zeshan.farooqi@vizrex.com>
2018-07-19 21:57:12 +0500

    First Check-in

---
+++ src/Console/Commands/ClearSession.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ *  Copyright © All Rights Reserved by Vizrex (Private) Limited 
+ *  Usage or redistribution of this code is strictly prohibited
+ *  without written consent of Vizrex (Private) Limited.
+ *  Queries are welcomed at copyright@vizrex.com
+ */
+
+namespace Vizrex\LaravelCleaner\Console\Commands;
+
+use RuntimeException;
+use Illuminate\Filesystem\Filesystem;
+use Vizrex\Laraviz\Console\Commands\BaseCommand;
+
+class ClearSession extends BaseCommand
+{    
+        
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'session:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear all session files';
+
+    /**
+     * The filesystem instance.
+     *
+     * @var \Illuminate\Filesystem\Filesystem
+     */
+    protected $files;
+
+    /**
+     * Create a new config clear command instance.
+     *
+     * @param  \Illuminate\Filesystem\Filesystem  $files
+     * @return void
+     */
+    public function __construct(Filesystem $files)
+    {
+        parent::__construct();
+
+        $this->files = $files;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $path = storage_path("framework/sessions");
+
+        if (! $path) {
+            throw new RuntimeException($this->str("path_not_found", ["path" => "Session"]));
+        }
+
+        foreach ($this->files->glob("{$path}/*") as $session) {
+            $this->files->delete($session);
+        }
+
+        $this->info($this->str("cleared", ["type" => "Session"]));
+    }
+    
+    protected function setNamespace()
+    {
+        $this->namespace = \Vizrex\LaravelCleaner\LaravelCleanerProvider::getNamespace();
+    }
+}
---
+++ src/resources/lang/en/strings.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ *  Copyright © All Rights Reserved by Vizrex (Private) Limited 
+ *  Usage or redistribution of this code is strictly prohibited
+ *  without written consent of Vizrex (Private) Limited.
+ *  Queries are welcomed at copyright@vizrex.com
+ */
+
+return [
+    "not_found" => ":command doesn't exist.",
+    "path_not_found" => ":path path not found!",
+    "cleared" => ":type files cleared!"
+];
